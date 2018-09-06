45290cdd8c110804dee24a3cfb3b8238ca8b6b62
Alessandro Mangiafico <40321580+r-seismic@users.noreply.github.com>
2018-09-05 19:49:52 -0600

    Update and rename AME Catalogue Subset Data.R to AME Catalogue - Subset Data.R

--- AME Catalogue - Subset Data.R
+++ AME Catalogue - Subset Data.R
@@ -1,16 +1,21 @@
 #Draw The Progress Bar
 pb <- winProgressBar(title = "AME Catalogue", label = "Initializing ", min = 0, max = 100, width = 300)
 
+#Loads stuff from rvars cache
+vars <- read.csv("vars.rvars")
+wd <- vars$directory
+
 #Loads Data from CSVs exported from ED Discovery
-setwd("C:/Users/every/Google Drive/Windows Mac Share/AME Catalogue")
+try(setwd(as.character(wd)))
 content_final <- read.csv("AME Catalogue (Final).csv")
 content_final$X.1 <- NULL
 
 setWinProgressBar(pb, 50, label = "Loading CSV 50% Total Completion ")
 
 #Subset Data By Properties
+#Put properties where it says "PUT PARAMETERS HERE", separated by commas
 #AND operator is &, OR operator is |
-results <- subset(content_final, content_final$Age.MY > 0)
+results <- subset(content_final, PUT PARAMETERS HERE)
 
 setWinProgressBar(pb, 100, label = "Subsetting Data 100% Total Completion ")
 
